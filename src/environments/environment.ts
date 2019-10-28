58eaecf0d04220fa7a16bc2dcfdcc9253f32c944
shocky2301 <sittisak.araya@gmail.com>
2019-10-28 10:48:39 +0700

    original cv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
    //API_URI: 'http://192.168.25.59:9101', // DEV P KG 2
   // API_URI: "http://192.168.35.120:4301", // QAS
   // API_URI: "http://192.168.15.44:4201", // PRD
-  // API_URI: "https://p3-bkd.buddyrecruit.ai", // PRD
+  API_URI: "https://p3-bkd.buddyrecruit.ai", // PRD
   // API_URI: "https://buddyrecruit.ai:4301", // PRD
-  API_URI: 'http://192.168.35.80:9001', // DEV N gos
+  // API_URI: 'http://192.168.35.92:9001', // DEV N gos
   WriteLog: true
 };
