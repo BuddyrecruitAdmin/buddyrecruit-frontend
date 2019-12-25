e54193584f53bbb0b14236963c390b02bc3fe4ba
SurasakMeetee <alex.cs.kku@gmail.com>
2019-12-25 11:23:40 +0700

    verify outlook

--- src/app/modules/verify/verify-outlook/verify-outlook.component.ts
+++ src/app/modules/verify/verify-outlook/verify-outlook.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ngx-verify-outlook',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyOutlookComponent implements OnInit {
 
-  constructor() { }
+  jsonFile: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getJSON().subscribe(data => {
+      this.jsonFile = data;
+    });
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get('../../../../assets/data/microsoft-identity-association.json');
   }
 
 }
