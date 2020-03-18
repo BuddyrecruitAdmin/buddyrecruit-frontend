43a372846583b78b6baa16c0c53db1cb5533e8c0
shocky2301 <sittisak.araya@gmail.com>
2020-03-18 16:58:48 +0700

    home

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   // API_URI: "http://localhost:4201", // LOCAL
   // API_URI: 'http://10.99.0.50:9001', // DEV P KG
-  API_URI: 'http://10.35.1.79:9001', // DEV N gos 15
+  API_URI: 'http://10.35.0.191:9001', // DEV N gos 15
   // API_URI: 'http://10.25.0.151:9001', // DEV N gos 6 
   // API_URI: 'http://192.168.35.87:9001', // DEV P'Mo
   WriteLog: true
