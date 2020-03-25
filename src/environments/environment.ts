2b7d6dc65f472f91eb8b2bfc112d50646460c92a
shocky2301 <sittisak.araya@gmail.com>
2020-03-25 15:01:37 +0700

    con true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //API_URI: 'http://35.247.152.36:4401', //PRD
   API_URI: 'https://p3bkd.buddyrecruit.ai', // DEV N gos 15
 
