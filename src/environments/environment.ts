ca54e5d815aabc4843eeb8e53ec6f7010132f122
shocky2301 <sittisak.araya@gmail.com>
2020-03-25 13:17:49 +0700

    config env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: true,
   //API_URI: 'http://35.247.152.36:4401', //PRD
-  API_URI: 'https://p3bkd.buddyrecruit.ai', //PRD
+  API_URI: 'https://p3bkd-qas.buddyrecruit.ai', //PRD
 
   WriteLog: true
 };
