59d3ab44064d3994f62f787b3b8f98d0a1d8c594
shocky4611 <sittisak.araya@gmail.com>
2020-04-28 17:33:29 +0700

    noti fix picture

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -176,7 +176,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
                 name: element.title,
                 title: this.utilitiesService.convertDateTimeFromSystem(element.date) ||
                   this.utilitiesService.convertDateTimeFromSystem(element.lastChangedInfo.date),
-                picture: element.fromUser.refUser.imageData,
+                picture: element.fromUser.refUser.imageData || 'https://image.flaticon.com/icons/png/512/55/55089.png',
                 hidden: element.readed
               });
             });
@@ -227,13 +227,23 @@ export class HeaderComponent implements OnInit, OnDestroy {
         }
         response.data.forEach(element => {
           this.notificationData.push(element);
-          this.notifications.push({
-            name: element.title,
-            title: this.utilitiesService.convertDateTimeFromSystem(element.date) ||
-              this.utilitiesService.convertDateTimeFromSystem(element.lastChangedInfo.date),
-            picture: element.fromUser.refUser.imageData,
-            hidden: element.readed
-          });
+          if (element.fromUser.refUser) {
+            this.notifications.push({
+              name: element.title,
+              title: this.utilitiesService.convertDateTimeFromSystem(element.date) ||
+                this.utilitiesService.convertDateTimeFromSystem(element.lastChangedInfo.date),
+              picture: element.fromUser.refUser.imageData || 'https://image.flaticon.com/icons/png/512/55/55089.png',
+              hidden: element.readed
+            });
+          } else {
+            this.notifications.push({
+              name: element.title,
+              title: this.utilitiesService.convertDateTimeFromSystem(element.date) ||
+                this.utilitiesService.convertDateTimeFromSystem(element.lastChangedInfo.date),
+              picture: 'https://image.flaticon.com/icons/png/512/55/55089.png',
+              hidden: element.readed
+            });
+          }
         });
         this.loading = false;
       }
