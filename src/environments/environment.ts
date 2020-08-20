1ffc3278fc0dabca85b98f1f56999ce849ecd68e
shocky4611 <sittisak.araya@gmail.com>
2020-08-20 18:06:57 +0700

    add appform for recruiter isdelete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  API_URI: "http://localhost:9002", //QAS
+  production: false, 
+  API_URI: "http://10.35.1.180:9001", //QAS
   WriteLog: true
 };
  
