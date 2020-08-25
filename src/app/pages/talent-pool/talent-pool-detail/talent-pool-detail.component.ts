57229342bf77c1f5fa8818a9d8ad1fed35286319
shocky4611 <sittisak.araya@gmail.com>
2020-08-25 16:49:50 +0700

    call track remove in prd

--- src/app/pages/talent-pool/talent-pool-detail/talent-pool-detail.component.ts
+++ src/app/pages/talent-pool/talent-pool-detail/talent-pool-detail.component.ts
@@ -1193,32 +1193,34 @@ export class TalentPoolDetailComponent implements OnInit {
   }
 
   changeCall(item) {
-    if (item.called.flag) {
-      const confirm = this.matDialog.open(PopupMessageComponent, {
-        width: `${this.utilitiesService.getWidthOfPopupCard()}px`,
-        data: { type: 'C', content: 'คุณต้องการยืนยันการโทรติดตามหรือไม่' }
-      });
-      confirm.afterClosed().subscribe(result => {
-        if (result) {
-          item.called.isFollow = true;
-          this.callService(item, item.called);
-        } else {
-          const confirm = this.matDialog.open(PopupMessageComponent, {
-            width: `${this.utilitiesService.getWidthOfPopupCard()}px`,
-            data: { type: 'C', content: 'คุณต้องการยกเลิกการโทรครั้งก่อนหรือไม่' }
-          });
-          confirm.afterClosed().subscribe(result => {
-            if (result) {
-              item.called.flag = !item.called.flag;
-              this.callService(item, item.called);
-            }
-          })
-        }
-      });
-    } else {
-      item.called.flag = !item.called.flag;
-      this.callService(item, item.called);
-    }
+    // if (item.called.flag) {
+    //   const confirm = this.matDialog.open(PopupMessageComponent, {
+    //     width: `${this.utilitiesService.getWidthOfPopupCard()}px`,
+    //     data: { type: 'C', content: 'คุณต้องการยืนยันการโทรติดตามหรือไม่' }
+    //   });
+    //   confirm.afterClosed().subscribe(result => {
+    //     if (result) {
+    //       item.called.isFollow = true;
+    //       this.callService(item, item.called);
+    //     } else {
+    //       const confirm = this.matDialog.open(PopupMessageComponent, {
+    //         width: `${this.utilitiesService.getWidthOfPopupCard()}px`,
+    //         data: { type: 'C', content: 'คุณต้องการยกเลิกการโทรครั้งก่อนหรือไม่' }
+    //       });
+    //       confirm.afterClosed().subscribe(result => {
+    //         if (result) {
+    //           item.called.flag = !item.called.flag;
+    //           this.callService(item, item.called);
+    //         }
+    //       })
+    //     }
+    //   });
+    // } else {
+    //   item.called.flag = !item.called.flag;
+    //   this.callService(item, item.called);
+    // }
+    item.called.flag = !item.called.flag;
+    this.callService(item, item.called);
   }
 
   callService(item, data) {
